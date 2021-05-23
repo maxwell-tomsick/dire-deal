a64108a2251138d1e7c811b372b8beee35a50c09
titan-mnemosyne <iainpile9@gmail.com>
2021-05-23 13:59:37 -0400

    minor fixes

--- source/App.cpp
+++ source/App.cpp
@@ -53,6 +53,11 @@ void LabApp::onStartup() {
          filetool::file_delete(Application::getSaveDirectory() + "savedGame.json");
     }
      */
+    if (!filetool::file_exists(Application::getSaveDirectory() + "settings.json")) {
+        std::shared_ptr<TextWriter> textWriter = TextWriter::alloc(Application::getSaveDirectory() + "settings.json");
+        textWriter->write("{\"Progress\":{\"HighestLevel\": 0},\"Volume\":{\"Music\":0.5,\"Sound\":0.5, \"Autoflip\":false}}");
+        textWriter->close();
+    }
     // Create a "loading" screen
     _itemChosen = false;
     _loaded = false;
@@ -67,11 +72,6 @@ void LabApp::onStartup() {
         _assets->loadDirectoryAsync("json/assets.json",nullptr);
     }
     AudioEngine::start();
-    if (!filetool::file_exists(Application::getSaveDirectory() + "settings.json")){
-        std::shared_ptr<TextWriter> textWriter = TextWriter::alloc(Application::getSaveDirectory() + "settings.json");
-        textWriter->write("{\"Progress\":{\"HighestLevel\": 0},\"Volume\":{\"Music\":0.5,\"Sound\":0.5, \"Autoflip\":false}}");
-        textWriter->close();
-    }
     //filetool::file_delete(Application::get()->getSaveDirectory() + "settings.json");
     //filetool::file_delete(Application::get()->getSaveDirectory() + "savedGame.json");
     /*
